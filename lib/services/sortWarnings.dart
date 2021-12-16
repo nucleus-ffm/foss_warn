8f233a0d9958b2ef84ecd6f48364b3f92cbbb40a
Nucleus-ffm <Nucleus-ffm@posteo.de>
2021-12-16 23:43:45 +0100

    add functions to convert severity and source to ints

--- lib/services/sortWarnings.dart
+++ lib/services/sortWarnings.dart
@@ -1,12 +1,41 @@
-import '../SettingsView.dart';
+import '../views/SettingsView.dart';
 import 'listHandler.dart';
+
+int convertSeverityToInt(String severity) {
+  switch (severity) {
+    case "Minor":
+      return 0;
+    case "Moderate":
+      return 1;
+    case "Extrem":
+      return 2;
+    case "Severe":
+      return 3;
+  }
+  return 0;
+}
+
+int convertSourceToInt(String source) {
+  switch (source) {
+    case "MOWAS":
+      return 0;
+    case "KATWARN":
+      return 1;
+    case "BIWAPP":
+      return 2;
+    case "DWD":
+      return 3;
+  }
+  return 0;
+}
+
 
 void sortWarnings() {
   if(sortWarningsBy == "severity") {
-    warnMessageList.sort((a, b) => b.severity.compareTo(a.severity));
+    warnMessageList.sort((a, b) => convertSeverityToInt(b.severity).compareTo(convertSeverityToInt(a.severity)));
   } else if(sortWarningsBy == "date") {
     warnMessageList.sort((a, b) => b.sent.compareTo(a.sent));
-  } else if(sortWarningsBy =="source") {
-    warnMessageList.sort((a, b) => b.publisher.compareTo(a.publisher));
+  } else if(sortWarningsBy == "source") {
+    warnMessageList.sort((a, b) => convertSourceToInt(b.publisher).compareTo(convertSourceToInt(a.publisher)));
   }
 }
