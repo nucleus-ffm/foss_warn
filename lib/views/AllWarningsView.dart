0cbebec7c823c701a96cd04c0491d26f0e7181ea
Nucleus-ffm <Nucleus-ffm@posteo.de>
2021-12-17 00:04:02 +0100

    move file

---
+++ lib/views/AllWarningsView.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+import 'package:foss_warn/widgets/ConnectionErrorWidget.dart';
+import 'package:provider/provider.dart';
+import '../main.dart';
+import '../services/getData.dart';
+import '../services/listHandler.dart';
+import '../widgets/WarningWidget.dart';
+import '../services/saveAndLoadSharedPreferences.dart';
+import '../services/sortWarnings.dart';
+import '../services/updateProvider.dart';
+
+class AllWarningsView extends StatefulWidget {
+  const AllWarningsView({Key? key}) : super(key: key);
+
+  @override
+  _AllWarningsViewState createState() => _AllWarningsViewState();
+}
+
+class _AllWarningsViewState extends State<AllWarningsView> {
+  var data;
+  bool loading = false;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    if (firstStart) {
+      loading = true;
+      firstStart = false;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Future<void> reloadData() async {
+      setState(() {
+        loading = true;
+      });
+      //
+    }
+
+    void loadData() async {
+      data = await getData();
+      sortWarnings();
+      loadNotificationSettingsImportanceList();
+      setState(() {
+        loading = false;
+      });
+    }
+
+    if (loading == true) {
+      loadData();
+    }
+    while (loading) {
+      // show loading screen
+      return Center(
+        child: SizedBox(
+          height: 70,
+          width: 70,
+          child: CircularProgressIndicator(
+            strokeWidth: 4,
+          ),
+        ),
+      );
+    }
+
+    return Consumer<Update>(
+      builder: (context, counter, child) => RefreshIndicator(
+        onRefresh: reloadData,
+        child: warnMessageList.isNotEmpty
+            ? SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Container(
+                      child: ConnectionError(),
+                    ),
+                    ...warnMessageList
+                        .map((warnMessage) => WarningWidget(warnMessage: warnMessage))
+                        .toList(),
+                  ]
+                ),
+              )
+            : Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Container(
+                        child: ConnectionError(),
+                      ),
+                    ],
+                  ),
+                  Expanded(
+                    child: Center(
+                      child: Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text("Hier gibt es noch nichts zu sehen... ",
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold)),
+                            Text("\n"),
+                            Text(
+                                "Entweder gibt es gerade keine Meldungen, \n oder Sie haben keine Internetverbindung?"),
+                            SizedBox(height: 10),
+                            TextButton(
+                              onPressed: () {
+                                setState(() {
+                                  loading = true;
+                                });
+                              },
+                              child: Text(
+                                "Neuladen",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              style: TextButton.styleFrom(
+                                  backgroundColor: Colors.blue),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+      ),
+    );
+  }
+}
