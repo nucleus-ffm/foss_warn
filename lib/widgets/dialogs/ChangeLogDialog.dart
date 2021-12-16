187899ba4451301b2d475c2f4dbbcd14d66a651d
Nucleus-ffm <Nucleus-ffm@posteo.de>
2021-12-17 00:13:30 +0100

    add changelog for 0.2.2

--- lib/widgets/dialogs/ChangeLogDialog.dart
+++ lib/widgets/dialogs/ChangeLogDialog.dart
@@ -14,12 +14,22 @@ class ChangeLogDialog extends StatelessWidget {
             mainAxisSize: MainAxisSize.min,
             children: [
               Text(
+                "0.2.2 (beta)",
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+              ),
+              Text("* Es gibt jetzt einen Darkmode \n"
+                  "* Die Einstellungen haben jetzt eine genauere Beschreibung \n"
+                  "* Die Tags bei den Meldungen werden jetzt umgebrochen \n"
+                  "* E-Mail Adressen in den Warntexten sind jetzt klickbar \n"
+                  "* Die Tags zur Warnstufe und Typ haben jetzt dynamische Farben \n"
+                  "* Die Meldungen können jetzt auch nach der Warnstufe sortiert werden \n"
+                  "* kleine Designverbesserungen \n"),
+              Text(
                 "0.2.1 (beta)",
                 style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
               ),
               Text("* Fehler bei der Schriftgröße korrigiert \n"
-                  "* Datenschutzdialog präzisiert "),
-              Text("\n"),
+                  "* Datenschutzdialog präzisiert \n "),
               Text(
                 "0.2.0 (beta)",
                 style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
@@ -34,8 +44,7 @@ class ChangeLogDialog extends StatelessWidget {
                       "* Die Schriftgröße der Meldungen kann jetzt angepasst werden \n"
                       "* Es gibt jetzt einen Updatechecker \n"
                       "* In Meldungen eingebettete Bilder können jetzt im Browser geöffnet werden \n"
-                      "* Links in den Warn-Texten sind jetzt klickbar"),
-              Text("\n"),
+                      "* Links in den Warn-Texten sind jetzt klickbar \n"),
               Text(
                 "0.1.13 (beta)",
                 style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
@@ -44,37 +53,45 @@ class ChangeLogDialog extends StatelessWidget {
                   "* unwichtige Tags können jetzt ausgeblendet werden\n" +
                   "* Warnungen können jetzt geteilt werden \n" +
                   "* Warnungen können jetzt auch mit einem Tip auf die Warnung geöffnet werden\n"
-                      "* Der Zeilenabstand bei der Liste der verfügbaren Orte wurde verringert"),
-              Text("\n"),
+                      "* Der Zeilenabstand bei der Liste der verfügbaren Orte wurde verringert \n"),
               Text(
                 "0.1.12 (beta)",
                 style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
               ),
+              Text("* Tippfehler verbessert \n"
+                  "* Möglichkeit zum Einstellen des Startbildschirms ergänzt \n"
+                  "* gleiche Benachrichtigungen werden jetzt nur einmal angezeigt \n"
+                  "* Benachrichtigungen werden jetzt richtig gruppiert  \n"
+                  "* Ort können jetzt auch mit dem Tippen auf den Ortsnamen geöffnet werden. \n"),
               Text(
-                  "* Tippfehler verbessert \n* Möglichkeit zum Einstellen des Startbildschirms ergänzt \n* gleiche Benachrichtigungen werden jetzt nur einmal angezeigt \n* Benachrichtigungen werden jetzt richtig gruppiert  \n* Ort können jetzt auch mit dem Tippen auf den Ortsnamen geöffnet werden."),
-              Text(
-                  "* Fehler mit doppelt angezeigten Warnungen bei 'meinen Orten' behoben \n * Pull to refresh bei 'Meine Orte' ergänzt \n* Benachrichtigungen werden jetzt abgebrochen, wenn die Warnung gelesen wurde - bei mehreren Warnungen für einen Ort durch die letzte Warnung in der Liste."),
-              Text("\n"),
+                  "* Fehler mit doppelt angezeigten Warnungen bei 'meinen Orten' behoben \n "
+                  "* Pull to refresh bei 'Meine Orte' ergänzt \n"
+                  "* Benachrichtigungen werden jetzt abgebrochen,"
+                  " wenn die Warnung gelesen wurde - bei mehreren Warnungen"
+                  " für einen Ort durch die letzte Warnung in der Liste. \n"),
               Text(
                 "0.1.11 (beta)",
                 style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
               ),
-              Text(
-                  "* Fehler behoben wodurch bei einer neuen Installation keine Benachrichtigungen angezeigt wurden.\n * kleine Layoutoptimierung für kleine Bildschirme."),
-              Text("\n"),
+              Text("* Fehler behoben wodurch bei einer neuen Installation"
+                  " keine Benachrichtigungen angezeigt wurden.\n "
+                  "* kleine Layoutoptimierung für kleine Bildschirme. \n"),
               Text(
                 "0.1.10 (beta)",
                 style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
               ),
-              Text(
-                  "* Änderungsprotokoll hinzugefügt\n* Statusanzeige um Fehler beim Auslesen erweitert \n* doppelte Orte aus der Liste entfernt\n * Falsche Zeitangabe korrigiert \n* Breche alle Benachrichtigungen ab, wenn eine Warnung gelesen wurde (noch nicht optimal)"),
-              Text("\n"),
+              Text("* Änderungsprotokoll hinzugefügt\n"
+                  "* Statusanzeige um Fehler beim Auslesen erweitert \n"
+                  "* doppelte Orte aus der Liste entfernt\n "
+                  "* Falsche Zeitangabe korrigiert \n"
+                  "* Breche alle Benachrichtigungen ab, wenn eine Warnung"
+                  " gelesen wurde (noch nicht optimal) \n"),
               Text(
                 "0.1.9 (beta)",
                 style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
               ),
-              Text(
-                  "* unnötige Schalter entfernt\n* Fehler korrigiert der das parsen abgebrochen hat"),
+              Text("* unnötige Schalter entfernt\n"
+                  "* Fehler korrigiert der das parsen abgebrochen hat"),
             ],
           ),
         ),
@@ -87,7 +104,6 @@ class ChangeLogDialog extends StatelessWidget {
           child: Text('schließen'),
         )
       ],
-      backgroundColor: Colors.white,
     );
   }
 }
