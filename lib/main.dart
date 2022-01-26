d6f862ad5966756a7b88519a5c5a2e0392b41e26
Nucleus-ffm <Nucleus-ffm@posteo.de>
2022-01-26 23:07:23 +0100

    new screenshots

--- lib/main.dart
+++ lib/main.dart
@@ -81,6 +81,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'FOSS Warn',
       theme: useDarkMode? ThemeData(
         primarySwatch: Colors.blue,
