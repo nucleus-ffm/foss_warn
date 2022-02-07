dc8675c67e1889d8a0ec3db9f8453a660b7477e3
Nucleus <Nucleus-ffm@posteo.de>
2022-02-07 22:59:16 +0100

    add information about the expire date

--- lib/class/class_WarnMessage.dart
+++ lib/class/class_WarnMessage.dart
@@ -14,6 +14,9 @@ class WarnMessage {
   String urgency;
   String severity;
   String certainty;
+  String effective; // just for DWD
+  String onset; // just for DWD
+  String expires; // just for DWD
   String headline;
   String description;
   String instruction;
@@ -24,27 +27,31 @@ class WarnMessage {
   String contact;
   String web;
 
-  WarnMessage(
-      {required this.identifier,
-      required this.publisher,
-      required this.source,
-      required this.sender,
-      required this.sent,
-      required this.status,
-      required this.messageTyp,
-      required this.scope,
-      required this.category,
-      required this.event,
-      required this.urgency,
-      required this.severity,
-      required this.certainty,
-      required this.headline,
-      required this.description,
-      required this.instruction,
-      required this.areaList,
-      //required this.area,
-      //required this.geocodeName,
-      //required this.geocodeNumber,
-      required this.contact,
-      required this.web});
+  WarnMessage({
+    required this.identifier,
+    required this.publisher,
+    required this.source,
+    required this.sender,
+    required this.sent,
+    required this.status,
+    required this.messageTyp,
+    required this.scope,
+    required this.category,
+    required this.event,
+    required this.urgency,
+    required this.severity,
+    required this.certainty,
+    this.effective = "",
+    this.onset = "",
+    this.expires = "",
+    required this.headline,
+    required this.description,
+    required this.instruction,
+    required this.areaList,
+    //required this.area,
+    //required this.geocodeName,
+    //required this.geocodeNumber,
+    required this.contact,
+    required this.web,
+  });
 }
