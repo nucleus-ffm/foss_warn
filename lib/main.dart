7bb7ecc9c0f2584c521afdb647f5c616d9abeadc
Nucleus <Nucleus-ffm@posteo.de>
2022-02-07 23:00:57 +0100

    add foreground service

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'dart:async';
+import 'dart:isolate';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_foreground_task/flutter_foreground_task.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/services.dart';
+//import 'package:connectivity_plus/connectivity_plus.dart';
+
 
 
 import 'widgets/SourceStatusWidget.dart';
@@ -12,6 +18,7 @@ import 'views/WelcomeView.dart';
 
 import 'class/class_NotificationService.dart';
 import 'class/class_BackgroundTask.dart';
+import 'class/class_ForegroundService.dart';
 
 import 'services/updateProvider.dart';
 import 'services/saveAndLoadSharedPreferences.dart';
@@ -23,6 +30,39 @@ import 'widgets/dialogs/SortByDialog.dart';
 //final navigatorKey = GlobalKey<NavigatorState>();
 GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
+// The callback function should always be a top-level function.
+void startCallback() {
+  // The setTaskHandler function must be called to handle the task in the background.
+  FlutterForegroundTask.setTaskHandler(FirstTaskHandler());
+}
+
+class FirstTaskHandler extends TaskHandler {
+  @override
+  Future<void> onStart(DateTime timestamp, SendPort? sendPort) async {
+    // You can use the getData function to get the data you saved.
+    final customData = await FlutterForegroundTask.getData<String>(key: 'customData');
+    print('customData: $customData');
+  }
+
+  @override
+  Future<void> onEvent(DateTime timestamp, SendPort? sendPort) async {
+    // Send data to the main isolate.
+    sendPort?.send(timestamp);
+  }
+
+  @override
+  Future<void> onDestroy(DateTime timestamp) async {
+    // You can use the clearAllData function to clear all the stored data.
+    await FlutterForegroundTask.clearAllData();
+  }
+
+  @override
+  void onButtonPressed(String id) {
+    // Called when the notification button on the Android platform is pressed.
+    print('onButtonPressed >> $id');
+  }
+}
+
 void main() async {
   // some initialisation
   WidgetsFlutterBinding.ensureInitialized();
@@ -33,11 +73,15 @@ void main() async {
   loadReadWarningsList(); // load the list with ids of read warnings
   await loadSettings(); // load settings / load the saved value of 'notificationGeneral'
 
+  // ForegroundService stuff
+  ForegroundService().initForegroundService();
+  ForegroundService().startForegroundServices();
+
   if (notificationGeneral) {
     // setup the background task
     print("Background notification enabled");
     BackgroundTaskManager().cancelBackgroundTask();
-    BackgroundTaskManager().registerBackgroundTask();
+    BackgroundTaskManager().registerBackgroundTaskWithDelay();
 
   } else {
     // the user do not want the background task
@@ -116,7 +160,7 @@ class MyApp extends StatelessWidget {
       ),
       //theme: ThemeData.dark(),
       navigatorKey: navigatorKey,
-      home: showWelcomeScreen ? WelcomeView() : ScaffoldView(),
+      home: WithForegroundTask(child: showWelcomeScreen ? WelcomeView() : ScaffoldView()),
     );
   }
 }
@@ -129,6 +173,8 @@ class ScaffoldView extends StatefulWidget {
 }
 
 class _ScaffoldViewState extends State<ScaffoldView> {
+  //late StreamSubscription<ConnectivityResult> _connectivitySubscription;
+
   int _selectedIndex = startScreen; // selected start view
   // the navigation bar
   void _onItemTapped(int index) {
@@ -151,7 +197,21 @@ class _ScaffoldViewState extends State<ScaffoldView> {
     loadMyPlacesList(); //load MyPlaceList
     listenNotifications();
     // listen to notification stream
+
+    /*_connectivitySubscription = Connectivity().onConnectivityChanged.listen((ConnectivityResult result) {
+      // Got a new connectivity status!
+      print("Connection changed");
+    });*/
   }
+
+  // cancel networt connection subscription
+  @override
+  dispose() {
+    super.dispose();
+
+    //_connectivitySubscription.cancel();
+  }
+
 
   void listenNotifications() {
     NotificationService.onNotification.stream.listen((onClickedNotification));
