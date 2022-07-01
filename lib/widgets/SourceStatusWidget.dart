55fc591ce9ff54d64d19e28adccb05b50fa93a35
Nucleus <Nucleus-ffm@posteo.de>
2022-07-01 12:34:46 +0200

    small UI fixes

--- lib/widgets/SourceStatusWidget.dart
+++ lib/widgets/SourceStatusWidget.dart
@@ -9,212 +9,214 @@ class StatusWidget extends StatelessWidget {
     return AlertDialog(
       title: Text('Statusanzeige'),
       content: Container(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              "Quellen Status:",
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(width: 130, child: Text("Mowas: ")),
-                SizedBox(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                "Quellen Status:",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(width: 130, child: Text("Mowas: ")),
+                  SizedBox(
+                      width: 30,
+                      child: mowasStatus
+                          ? mowasParseStatus
+                              ? Icon(
+                                  Icons.check_box,
+                                  color: Colors.green,
+                                )
+                              : Icon(
+                                  Icons.speaker_notes_off_outlined,
+                                  color: Colors.red,
+                                )
+                          : Icon(
+                              Icons.error,
+                              color: Colors.red,
+                            )),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(width: 130, child: Text("Katwarn: ")),
+                  SizedBox(
+                      width: 30,
+                      child: katwarnStatus
+                          ? katwarnParseStatus
+                              ? Icon(
+                                  Icons.check_box,
+                                  color: Colors.green,
+                                )
+                              : Icon(
+                                  Icons.speaker_notes_off_outlined,
+                                  color: Colors.red,
+                                )
+                          : Icon(Icons.error, color: Colors.red)),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(width: 130, child: Text("Biwapp: ")),
+                  SizedBox(
+                      width: 30,
+                      child: biwappStatus
+                          ? biwappParseStatus
+                              ? Icon(
+                                  Icons.check_box,
+                                  color: Colors.green,
+                                )
+                              : Icon(
+                                  Icons.speaker_notes_off_outlined,
+                                  color: Colors.red,
+                                )
+                          : Icon(Icons.error, color: Colors.red)),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(width: 130, child: Text("DWD: ")),
+                  SizedBox(
+                      width: 30,
+                      child: dwdStatus
+                          ? dwdParseStatus
+                              ? Icon(
+                                  Icons.check_box,
+                                  color: Colors.green,
+                                )
+                              : Icon(
+                                  Icons.speaker_notes_off_outlined,
+                                  color: Colors.red,
+                                )
+                          : Icon(Icons.error, color: Colors.red)),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(width: 130, child: Text("LHP: ")),
+                  SizedBox(
+                      width: 30,
+                      child: lhpStatus
+                          ? lhpParseStatus
+                          ? Icon(
+                        Icons.check_box,
+                        color: Colors.green,
+                      )
+                          : Icon(
+                        Icons.speaker_notes_off_outlined,
+                        color: Colors.red,
+                      )
+                          : Icon(Icons.error, color: Colors.red)),
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                      width: 30, child: Icon(Icons.error, color: Colors.red)),
+                  SizedBox(
+                    width: 140,
+                    child: Text(
+                      " = Server nicht erreichbar",
+                      style: TextStyle(fontSize: 12),
+                    ),
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                      width: 30,
+                      child: Icon(Icons.speaker_notes_off_outlined,
+                          color: Colors.red)),
+                  SizedBox(
+                    width: 140,
+                    child: Text(
+                      " = Fehler beim Auslesen",
+                      style: TextStyle(fontSize: 12),
+                    ),
+                  )
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
                     width: 30,
-                    child: mowasStatus
-                        ? mowasParseStatus
-                            ? Icon(
-                                Icons.check_box,
-                                color: Colors.green,
-                              )
-                            : Icon(
-                                Icons.speaker_notes_off_outlined,
-                                color: Colors.red,
-                              )
-                        : Icon(
-                            Icons.error,
-                            color: Colors.red,
-                          )),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(width: 130, child: Text("Katwarn: ")),
-                SizedBox(
-                    width: 30,
-                    child: katwarnStatus
-                        ? katwarnParseStatus
-                            ? Icon(
-                                Icons.check_box,
-                                color: Colors.green,
-                              )
-                            : Icon(
-                                Icons.speaker_notes_off_outlined,
-                                color: Colors.red,
-                              )
-                        : Icon(Icons.error, color: Colors.red)),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(width: 130, child: Text("Biwapp: ")),
-                SizedBox(
-                    width: 30,
-                    child: biwappStatus
-                        ? biwappParseStatus
-                            ? Icon(
-                                Icons.check_box,
-                                color: Colors.green,
-                              )
-                            : Icon(
-                                Icons.speaker_notes_off_outlined,
-                                color: Colors.red,
-                              )
-                        : Icon(Icons.error, color: Colors.red)),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(width: 130, child: Text("DWD: ")),
-                SizedBox(
-                    width: 30,
-                    child: dwdStatus
-                        ? dwdParseStatus
-                            ? Icon(
-                                Icons.check_box,
-                                color: Colors.green,
-                              )
-                            : Icon(
-                                Icons.speaker_notes_off_outlined,
-                                color: Colors.red,
-                              )
-                        : Icon(Icons.error, color: Colors.red)),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(width: 130, child: Text("LHP: ")),
-                SizedBox(
-                    width: 30,
-                    child: lhpStatus
-                        ? lhpParseStatus
-                        ? Icon(
+                    child: Icon(
                       Icons.check_box,
                       color: Colors.green,
-                    )
-                        : Icon(
-                      Icons.speaker_notes_off_outlined,
-                      color: Colors.red,
-                    )
-                        : Icon(Icons.error, color: Colors.red)),
-              ],
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                    width: 30, child: Icon(Icons.error, color: Colors.red)),
-                SizedBox(
-                  width: 140,
-                  child: Text(
-                    " = Server nicht erreichbar",
-                    style: TextStyle(fontSize: 12),
+                    ),
                   ),
-                )
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                    width: 30,
-                    child: Icon(Icons.speaker_notes_off_outlined,
-                        color: Colors.red)),
-                SizedBox(
-                  width: 140,
-                  child: Text(
-                    " = Fehler beim Auslesen",
-                    style: TextStyle(fontSize: 12),
-                  ),
-                )
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  width: 30,
-                  child: Icon(
-                    Icons.check_box,
-                    color: Colors.green,
-                  ),
-                ),
-                SizedBox(
-                  width: 140,
-                  child: Text(
-                    " = alles ok",
-                    style: TextStyle(fontSize: 12),
-                  ),
-                )
-              ],
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            Text(
-              "Anzahl der Meldungen:",
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(width: 130, child: Text("Mowas:")),
-                SizedBox(width: 30, child: Text(mowasMessages.toString())),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(width: 130, child: Text("Katwarn: ")),
-                SizedBox(width: 30, child: Text(katwarnMessages.toString())),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(width: 130, child: Text("Biwapp:")),
-                SizedBox(width: 30, child: Text(biwappMessages.toString())),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(width: 130, child: Text("DWD:")),
-                SizedBox(width: 30, child: Text(dwdMessages.toString())),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(width: 130, child: Text("LHP:")),
-                SizedBox(width: 30, child: Text(lhpMessages.toString())),
-              ],
-            ),
-          ],
+                  SizedBox(
+                    width: 140,
+                    child: Text(
+                      " = alles ok",
+                      style: TextStyle(fontSize: 12),
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Text(
+                "Anzahl der Meldungen:",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(width: 130, child: Text("Mowas:")),
+                  SizedBox(width: 30, child: Text(mowasMessages.toString())),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(width: 130, child: Text("Katwarn: ")),
+                  SizedBox(width: 30, child: Text(katwarnMessages.toString())),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(width: 130, child: Text("Biwapp:")),
+                  SizedBox(width: 30, child: Text(biwappMessages.toString())),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(width: 130, child: Text("DWD:")),
+                  SizedBox(width: 30, child: Text(dwdMessages.toString())),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(width: 130, child: Text("LHP:")),
+                  SizedBox(width: 30, child: Text(lhpMessages.toString())),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
       actions: <Widget>[
