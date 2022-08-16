5f06713a4f1678b24bd6a2a3d89bc39c7355ea9b
Nucleus-ffm <Nucleus-ffm@posteo.de>
2022-08-16 20:55:17 +0200

    add joJson and FromJson to store places

--- lib/class/class_Place.dart
+++ lib/class/class_Place.dart
@@ -2,9 +2,19 @@ import 'class_WarnMessage.dart';
 
 class Place {
   String name;
+  String geocode;
   dynamic countWarnings = 0;
   List<WarnMessage> warnings = [];
   List<WarnMessage> alreadyReadWarnings = [];
 
-  Place( {required this.name});
+  Place( {required this.name, required this.geocode});
+
+  Place.fromJson(Map<String, dynamic> json)
+      : name = json['name'],
+        geocode = json['geocode'];
+
+  Map<String, dynamic> toJson() => {
+    'name': name,
+    'geocode': geocode,
+  };
 }
