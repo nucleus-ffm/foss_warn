7507fe3c316d7dbc07b6e1062cc47bea6a605195
Nucleus <Nucleus-ffm@posteo.de>
2022-08-16 21:56:17 +0200

    add print

--- lib/services/legacyHandler.dart
+++ lib/services/legacyHandler.dart
@@ -2,6 +2,7 @@ import 'package:foss_warn/services/saveAndLoadSharedPreferences.dart';
 
 /// fix braking changes in old releases
 void legacyHandler() {
+  print("[legacy handler]");
 
   // fix change of the ImportanceList. It is now lowercase because of alert swiss
   saveNotificationSettingsImportanceList();
