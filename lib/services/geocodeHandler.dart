32fce15263032f68d82184814dcb5a6c94ff810d
Nucleus-ffm <Nucleus-ffm@posteo.de>
2022-08-16 20:53:56 +0200

    add new methode to add places

---
+++ lib/services/geocodeHandler.dart
@@ -0,0 +1,107 @@
+import 'dart:convert';
+import 'package:foss_warn/services/listHandler.dart';
+import 'package:foss_warn/services/saveAndLoadSharedPreferences.dart';
+import 'package:http/http.dart';
+
+import 'allPlacesList.dart';
+
+Future<void> geocodeHandler() async {
+  print("[geocodehandler]");
+  var url = "https://www.xrepository.de/api/xrepository/urn:de:bund:destatis"
+      ":bevoelkerungsstatistik:schluessel:rs_2021-07-31/download/"
+      "Regionalschl_ssel_2021-07-31.json";
+  Response response; //response var for get request
+  var data; //var for response data
+  try {
+    String? savedData = await loadGeocode();
+    if(savedData != null) {
+      // we don't need to laod the data from web
+      print("[geocodeHandler] data already stored");
+      data = savedData;
+    } else {
+      response = await get(Uri.parse(url));
+      if (response.statusCode == 200) {
+        print("[geocodehanlder] got data ");
+        data = jsonDecode(utf8.decode(response.bodyBytes));
+      }
+    }
+    if(data != null) {
+      allAvailablePlacesNames.clear();
+
+      for (int j = 0; j < data["daten"].length; j++) {
+        geocodeMap.putIfAbsent(data["daten"][j][1], () => data["daten"][j][0]);
+        allAvailablePlacesNames.add(data["daten"][j][1]);
+      }
+
+      // add alert swiss places to list
+      createAlertSwissPlacesMap();
+
+      print("[geocodehanlder] finish");
+    } else {
+      print("could not reach geocode source");
+    }
+  } catch (e) {
+    print("[geocodehandler] something went wrong: " + e.toString());
+  }
+}
+
+/// create map with short name and full name and create List with full name
+void createAlertSwissPlacesMap() {
+  alertSwissPlacesMap.putIfAbsent("Zürich", () => "ZH");
+  alertSwissPlacesMap.putIfAbsent("Bern", () => "BE");
+  alertSwissPlacesMap.putIfAbsent("Luzern", () => "LU");
+  alertSwissPlacesMap.putIfAbsent("Uri", () => "UR");
+  alertSwissPlacesMap.putIfAbsent("Schwyz", () => "SZ");
+  alertSwissPlacesMap.putIfAbsent("Obwalden", () => "OW");
+  alertSwissPlacesMap.putIfAbsent("Nidwalden", () => "NW");
+  alertSwissPlacesMap.putIfAbsent("Glarus", () => "GL");
+  alertSwissPlacesMap.putIfAbsent("Zug", () => "ZG");
+  alertSwissPlacesMap.putIfAbsent("Freiburg", () => "FR");
+  alertSwissPlacesMap.putIfAbsent("Solothurn", () => "SO");
+  alertSwissPlacesMap.putIfAbsent("Basel-Stadt", () => "BS");
+  alertSwissPlacesMap.putIfAbsent("Basel-Landschaft", () => "BL");
+  alertSwissPlacesMap.putIfAbsent("Schaffhausen", () => "SH");
+  alertSwissPlacesMap.putIfAbsent("Appenzell Ausserrhoden", () => "AR");
+  alertSwissPlacesMap.putIfAbsent("Appenzell Innerrhoden", () => "AI");
+  alertSwissPlacesMap.putIfAbsent("St. Gallen", () => "SG");
+  alertSwissPlacesMap.putIfAbsent("Graubünden", () => "GR");
+  alertSwissPlacesMap.putIfAbsent("Aargau", () => "AG");
+  alertSwissPlacesMap.putIfAbsent("Thurgau", () => "TG");
+  alertSwissPlacesMap.putIfAbsent("Ticino", () => "TI");
+  alertSwissPlacesMap.putIfAbsent("Vaud", () => "VD");
+  alertSwissPlacesMap.putIfAbsent("Valais", () => "VS");
+  alertSwissPlacesMap.putIfAbsent("Neuchâtel", () => "NE");
+  alertSwissPlacesMap.putIfAbsent("Genève", () => "GE");
+  alertSwissPlacesMap.putIfAbsent("Jura", () => "JU");
+
+  alertSwissPlacesList.clear();
+  alertSwissPlacesList.addAll([
+    "Zürich",
+    "Bern",
+    "Luzern",
+    "Uri",
+    "Schwyz",
+    "Obwalden",
+    "Nidwalden",
+    "Glarus",
+    "Zug",
+    "Freiburg",
+    "Solothurn",
+    "Basel-Stadt",
+    "Basel-Landschaft",
+    "Schaffhausen",
+    "Appenzell Ausserrhoden",
+    "Appenzell Innerrhoden",
+    "St. Gallen",
+    "Graubünden",
+    "Aargau",
+    "Thurgau",
+    "Ticino",
+    "Vaud",
+    "Valais",
+    "Neuchâtel",
+    "Genève",
+    "Jura"
+  ]);
+  allAvailablePlacesNames.addAll(alertSwissPlacesList);
+}
---
+++ lib/views/addMyPlaceView.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:foss_warn/services/listHandler.dart';
+import 'package:provider/provider.dart';
+import '../services/updateProvider.dart';
+
+
+
+class AddMyPlaceView extends StatefulWidget {
+  const AddMyPlaceView({Key? key}) : super(key: key);
+
+  @override
+  State<AddMyPlaceView> createState() => _AddMyPlaceViewState();
+}
+
+class _AddMyPlaceViewState extends State<AddMyPlaceView> {
+  String newPlaceName = "";
+  List<String> allPlacesToShow = [];
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Ort hinzufügen"),
+        backgroundColor: Colors.green[700],
+        systemOverlayStyle:
+        SystemUiOverlayStyle(statusBarBrightness: Brightness.dark),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            //mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                cursorColor: Theme.of(context).colorScheme.secondary,
+
+                autofocus: true,
+                decoration: new InputDecoration(
+                  labelText: 'Ortsname oder Kreisname',
+                  labelStyle: TextStyle(
+                      color: Theme.of(context).colorScheme.secondary
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Theme.of(context).colorScheme.secondary),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Theme.of(context).colorScheme.secondary),
+                  ),
+                  border: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Theme.of(context).colorScheme.secondary),
+                  ),
+                ),
+                onChanged: (text) {
+                  newPlaceName = text;
+                  text = text.toLowerCase();
+                  setState(() {
+                    allPlacesToShow = allAvailablePlacesNames.where((place) {
+                      var search = place.toLowerCase();
+                      return search.contains(text);
+                    }).toList();
+                  });
+                },
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "Die folgende Liste könnte Fehlerhaft sein, wodurch es zu keiner"
+                    " Warnung kommt. Wenn Fehler auffallen, bitte Bescheid geben.",
+                style: TextStyle(fontSize: 10),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              SizedBox(
+                  height: MediaQuery.of(context).size.height,
+                child: ListView(
+                  children: allPlacesToShow
+                      .map(
+                        (place) => ListTile(
+                      visualDensity: VisualDensity(horizontal: 0, vertical: -4),
+                      title: Text(place),
+                      onTap: () {
+                        setState(() {
+                          final updater =
+                          Provider.of<Update>(context, listen: false);
+                          updater.updateList(place);
+                          Navigator.of(context).pop();
+                        });
+                      },
+                    ),
+                  )
+                      .toList(),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
