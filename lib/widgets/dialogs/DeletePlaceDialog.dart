cb9535ef69c6fa60327dae1b18572ac7ec5e9414
Nucleus <Nucleus-ffm@posteo.de>
2022-12-27 21:31:25 +0100

    new translation

--- lib/widgets/dialogs/DeletePlaceDialog.dart
+++ lib/widgets/dialogs/DeletePlaceDialog.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import '../../class/class_Place.dart';
 import '../../services/updateProvider.dart';
 import 'package:provider/provider.dart';
@@ -12,16 +13,15 @@ class DeletePlaceDialog extends StatefulWidget {
 }
 
 class _DeletePlaceDialogState extends State<DeletePlaceDialog> {
-  String newPlaceName = "";
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      title: Text('Ort löschen?'),
+      title: Text(AppLocalizations.of(context).delete_place_headline),
       content: Container(
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            Text("Bist Du sicher, dass Du diesen Ort löschen möchtest?"),
+            Text(AppLocalizations.of(context).delete_place_confirmation),
           ],
         ),
       ),
@@ -30,18 +30,23 @@ class _DeletePlaceDialogState extends State<DeletePlaceDialog> {
           onPressed: () {
             Navigator.of(context).pop();
           },
-          child: Text('Abbrechen', style: TextStyle(color: Colors.red),),
+          child: Text(
+            AppLocalizations.of(context).delete_place_cancel,
+            style: TextStyle(color: Colors.red),
+          ),
         ),
         new TextButton(
           onPressed: () {
             //remove place from list and update view
             print("place deleted");
-            final updater =
-            Provider.of<Update>(context, listen: false);
+            final updater = Provider.of<Update>(context, listen: false);
             updater.deletePlace(widget.myPlace);
             Navigator.of(context).pop();
           },
-          child: Text('Löschen', style: TextStyle(color: Colors.green),),
+          child: Text(
+            AppLocalizations.of(context).delete_place_delete,
+            style: TextStyle(color: Colors.green),
+          ),
         )
       ],
     );
