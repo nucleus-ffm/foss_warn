e3737916743868855d7d57670c8c38cb3d95a767
Nucleus <Nucleus-ffm@posteo.de>
2023-01-14 18:18:19 +0100

    move _isBatteryOptimizationEnabled to function to catch exceptions

--- lib/services/collectSysteminfo.dart
+++ lib/services/collectSysteminfo.dart
@@ -2,10 +2,21 @@ import 'package:flutter/services.dart';
 
 /// collect all available infos to help finding Bugs and wrong settings
 Future<String> collectSystemInfo() async {
-  final _platform = const MethodChannel('flutter.native/helper');
+
   String result  = "Systeminformationen \n \n";
 
-  result+= "Akkuoptimierung: "  + (await _platform.invokeMethod("isBatteryOptimizationEnabled")).toString() + "\n ";
+  result+= "Akkuoptimierung: ${_isBatteryOptimizationEnabled()} \n ";
 
   return result;
 }
+
+Future<bool> _isBatteryOptimizationEnabled() async {
+  final _platform = const MethodChannel('flutter.native/helper');
+  try {
+    return await _platform.invokeMethod("isBatteryOptimizationEnabled");
+  } on PlatformException catch (e) {
+    print(e);
+    return false;
+  }
+}
+
