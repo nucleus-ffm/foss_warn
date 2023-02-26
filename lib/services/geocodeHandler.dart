038847b17f598eabdd94aa46c91091dac6c721ba
Nucleus <Nucleus-ffm@posteo.de>
2023-02-26 21:45:30 +0100

    use nina place and alert swiss place class to create new Places

--- lib/services/geocodeHandler.dart
+++ lib/services/geocodeHandler.dart
@@ -1,17 +1,22 @@
 import 'dart:convert';
-import 'package:foss_warn/class/class_Place.dart';
+import 'package:foss_warn/class/class_AlertSwissPlace.dart';
+import 'package:foss_warn/class/class_Geocode.dart';
+import 'package:foss_warn/class/class_NinaPlace.dart';
 import 'package:foss_warn/services/listHandler.dart';
 import 'package:foss_warn/services/saveAndLoadSharedPreferences.dart';
 import 'package:http/http.dart';
 
+import '../class/abstract_Place.dart';
 import 'allPlacesList.dart';
-import 'extractStateNameFromGeocode.dart';
+
 
 const String url =
     "https://www.xrepository.de/api/xrepository/urn:de:bund:destatis"
     ":bevoelkerungsstatistik:schluessel:rs_2021-07-31/download/"
     "Regionalschl_ssel_2021-07-31.json";
 
+
+//  @todo: move to geocode class?
 Future<void> geocodeHandler() async {
   print("[geocodehandler]");
 
@@ -26,19 +31,20 @@ Future<void> geocodeHandler() async {
     allAvailablePlacesNames.clear();
 
     for (int i = 0; i < data["daten"].length; i++) {
-      final place =
-          Place(name: data["daten"][i][1], geocode: data["daten"][i][0]);
+      // print( "name:" +  data["daten"][i][1] + "geocodeNumber:" +  data["daten"][i][0]);
+      Place place =
+          NinaPlace(name: data["daten"][i][1], geocode: Geocode(geocodeNumber: data["daten"][i][0], geocodeName: ""));
 
       // we can not receive any warning for OT (Ortsteile)
       if (place.name.contains("OT")) continue;
 
-      final placeNameWithState =
-          "${place.name} ${extractStateNameFromGeocode(place.geocode)}";
+      /* final placeNameWithState =
+          "${place.name} ${place.geocode.stateName}"; */
 
       // add to map only used in background
-      geocodeMap.putIfAbsent(placeNameWithState, () => place.geocode);
+      // geocodeMap.putIfAbsent(placeNameWithState, () => place.geocode.geocodeName);
       // add to list, used to display the Places List
-      allAvailablePlacesNames.add(placeNameWithState);
+      allAvailablePlacesNames.add(place);
     }
 
     // add alert swiss places to list
@@ -71,61 +77,33 @@ Future<dynamic> getPlaces() async {
 
 /// create map with short name and full name and create List with full name
 void createAlertSwissPlacesMap() {
-  alertSwissPlacesMap.putIfAbsent("Zürich", () => "ZH");
-  alertSwissPlacesMap.putIfAbsent("Bern", () => "BE");
-  alertSwissPlacesMap.putIfAbsent("Luzern", () => "LU");
-  alertSwissPlacesMap.putIfAbsent("Uri", () => "UR");
-  alertSwissPlacesMap.putIfAbsent("Schwyz", () => "SZ");
-  alertSwissPlacesMap.putIfAbsent("Obwalden", () => "OW");
-  alertSwissPlacesMap.putIfAbsent("Nidwalden", () => "NW");
-  alertSwissPlacesMap.putIfAbsent("Glarus", () => "GL");
-  alertSwissPlacesMap.putIfAbsent("Zug", () => "ZG");
-  alertSwissPlacesMap.putIfAbsent("Freiburg", () => "FR");
-  alertSwissPlacesMap.putIfAbsent("Solothurn", () => "SO");
-  alertSwissPlacesMap.putIfAbsent("Basel-Stadt", () => "BS");
-  alertSwissPlacesMap.putIfAbsent("Basel-Landschaft", () => "BL");
-  alertSwissPlacesMap.putIfAbsent("Schaffhausen", () => "SH");
-  alertSwissPlacesMap.putIfAbsent("Appenzell Ausserrhoden", () => "AR");
-  alertSwissPlacesMap.putIfAbsent("Appenzell Innerrhoden", () => "AI");
-  alertSwissPlacesMap.putIfAbsent("St. Gallen", () => "SG");
-  alertSwissPlacesMap.putIfAbsent("Graubünden", () => "GR");
-  alertSwissPlacesMap.putIfAbsent("Aargau", () => "AG");
-  alertSwissPlacesMap.putIfAbsent("Thurgau", () => "TG");
-  alertSwissPlacesMap.putIfAbsent("Ticino", () => "TI");
-  alertSwissPlacesMap.putIfAbsent("Vaud", () => "VD");
-  alertSwissPlacesMap.putIfAbsent("Valais", () => "VS");
-  alertSwissPlacesMap.putIfAbsent("Neuchâtel", () => "NE");
-  alertSwissPlacesMap.putIfAbsent("Genève", () => "GE");
-  alertSwissPlacesMap.putIfAbsent("Jura", () => "JU");
 
   alertSwissPlacesList.clear();
-  alertSwissPlacesList.addAll([
-    "Zürich",
-    "Bern",
-    "Luzern",
-    "Uri",
-    "Schwyz",
-    "Obwalden",
-    "Nidwalden",
-    "Glarus",
-    "Zug",
-    "Freiburg",
-    "Solothurn",
-    "Basel-Stadt",
-    "Basel-Landschaft",
-    "Schaffhausen",
-    "Appenzell Ausserrhoden",
-    "Appenzell Innerrhoden",
-    "St. Gallen",
-    "Graubünden",
-    "Aargau",
-    "Thurgau",
-    "Ticino",
-    "Vaud",
-    "Valais",
-    "Neuchâtel",
-    "Genève",
-    "Jura"
-  ]);
-  allAvailablePlacesNames.addAll(alertSwissPlacesList);
+
+  allAvailablePlacesNames.add(AlertSwissPlace(name: "Zürich", shortName: "ZH"));
+  allAvailablePlacesNames.add(AlertSwissPlace(name: "Bern", shortName: "BE"));
+  allAvailablePlacesNames.add(AlertSwissPlace(name: "Luzern", shortName: "LU"));
+  allAvailablePlacesNames.add(AlertSwissPlace(name: "Uri", shortName: "UR"));
+  allAvailablePlacesNames.add(AlertSwissPlace(name: "Schwyz", shortName: "SZ"));
+  allAvailablePlacesNames.add(AlertSwissPlace(name: "Obwalden", shortName: "OW"));
+  allAvailablePlacesNames.add(AlertSwissPlace(name: "Nidwalden", shortName: "NW"));
+  allAvailablePlacesNames.add(AlertSwissPlace(name: "Glarus", shortName: "GL"));
+  allAvailablePlacesNames.add(AlertSwissPlace(name: "Zug", shortName: "ZG"));
+  allAvailablePlacesNames.add(AlertSwissPlace(name: "Freiburg", shortName: "FR"));
+  allAvailablePlacesNames.add(AlertSwissPlace(name: "Solothurn", shortName: "SO"));
+  allAvailablePlacesNames.add(AlertSwissPlace(name: "Basel-Stadt", shortName: "BS"));
+  allAvailablePlacesNames.add(AlertSwissPlace(name: "Basel-Landschaft", shortName: "BL"));
+  allAvailablePlacesNames.add(AlertSwissPlace(name: "Schaffhausen", shortName: "SH"));
+  allAvailablePlacesNames.add(AlertSwissPlace(name: "Appenzell Ausserrhoden", shortName: "AR"));
+  allAvailablePlacesNames.add(AlertSwissPlace(name: "Appenzell Innerrhoden", shortName: "AI"));
+  allAvailablePlacesNames.add(AlertSwissPlace(name: "St. Gallen", shortName: "SG"));
+  allAvailablePlacesNames.add(AlertSwissPlace(name: "Graubünden", shortName: "GR"));
+  allAvailablePlacesNames.add(AlertSwissPlace(name: "Aargau", shortName: "AG"));
+  allAvailablePlacesNames.add(AlertSwissPlace(name: "Thurgau", shortName: "TG"));
+  allAvailablePlacesNames.add(AlertSwissPlace(name: "Ticino", shortName: "TI"));
+  allAvailablePlacesNames.add(AlertSwissPlace(name: "Vaud", shortName: "VD"));
+  allAvailablePlacesNames.add(AlertSwissPlace(name: "Valais", shortName: "VS"));
+  allAvailablePlacesNames.add(AlertSwissPlace(name: "Neuchâtel", shortName: "NE"));
+  allAvailablePlacesNames.add(AlertSwissPlace(name: "Genève", shortName: "GE"));
+  allAvailablePlacesNames.add(AlertSwissPlace(name: "Jura", shortName: "JU"));
 }
