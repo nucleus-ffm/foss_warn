79ca6980d69032a41a971b3d9a99ccc75d6a2f01
Nucleus <nucleus-ffm@posteo.de>
2023-12-20 23:33:35 +0100

    hide alert swiss settings, if disabled

--- lib/widgets/NotificationPreferencesListTileWidget.dart
+++ lib/widgets/NotificationPreferencesListTileWidget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:foss_warn/main.dart';
 import '../class/class_notificationPreferences.dart';
 import '../enums/NotificationLevel.dart';
 import '../enums/WarningSource.dart';
@@ -117,119 +118,129 @@ class _NotificationPreferencesListTileWidgetState
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      contentPadding: settingsTileListPadding,
-      title: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(
-            widget.notificationPreferences.warningSource.name.toUpperCase(),
-            style: Theme.of(context).textTheme.titleMedium,
-          ),
-          // show a switch when source can be disabled
-          SourceCanBeDisabled(widget.notificationPreferences.warningSource)
-              ? Switch(
-                  value: widget.notificationPreferences.notificationLevel !=
-                      NotificationLevel.disabled,
-                  onChanged: (value) {
-                    // if source is enabled, set notificationLevel to getUpToMinor
-                    // if source is disabled set notification level to disabled,
-                    // the slider will be hidden when notification
-                    // level is set to disabled
-                    if (value) {
-                      setState(() {
-                        widget.notificationPreferences.notificationLevel =
-                            NotificationLevel.getUpToMinor;
-                      });
-                    } else {
-                      setState(() {
-                        widget.notificationPreferences.notificationLevel =
-                            NotificationLevel.disabled;
-                      });
-                    }
-                    saveSettings();
-                  },
-                )
-              : SizedBox(),
-        ],
-      ),
-      subtitle: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-              getDescriptionForEventSetting(
-                  widget.notificationPreferences.warningSource),
-              style: Theme.of(context).textTheme.bodyMedium),
-          // hide the slider when source is disabled
-          SourceCanBeDisabled(widget.notificationPreferences.warningSource) &&
-                  widget.notificationPreferences.notificationLevel ==
-                      NotificationLevel.disabled
-              ? Container(
-                  padding: EdgeInsets.only(top: 5),
-                  child: Text(
-                      "Source disabled - you won't get a notification"), //@todo translate
-                )
-              : Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      mainAxisSize: MainAxisSize.max,
-                      children: [
-                        Icon(
-                          Icons.notifications_active,
-                          color: Colors.red,
-                        ),
-                        Flexible(
-                          child: Slider(
-                            label: getLabelForWarningSeverity(
-                                getValueForWarningLevel(widget
-                                    .notificationPreferences
-                                    .notificationLevel)),
-                            divisions: 3,
-                            min: 0,
-                            max: 3,
-                            value: getValueForWarningLevel(widget
-                                .notificationPreferences.notificationLevel),
-                            onChanged: (value) {
-                              setState(
-                                () {
-                                  print(widget.notificationPreferences
-                                      .warningSource.name);
-                                  // update notification level with slider value
-                                  widget.notificationPreferences
-                                          .notificationLevel =
-                                      getNotificationPreferencesForValue(value);
-                                },
-                              );
-                            },
-                            onChangeEnd: (value) {
-                              // save settings, after change is complete
-                              saveSettings();
-                            },
-                          ),
-                        ),
-                        Icon(
-                          Icons.notifications,
-                          color: Colors.orangeAccent,
-                        ),
-                      ],
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 30, right: 30),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return widget.notificationPreferences.warningSource ==
+                WarningSource.alertSwiss &&
+            !userPreferences.activateAlertSwiss
+        ? SizedBox() // hide alert swiss if not activated
+        : ListTile(
+            contentPadding: settingsTileListPadding,
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  widget.notificationPreferences.warningSource.name
+                      .toUpperCase(),
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
+                // show a switch when source can be disabled
+                SourceCanBeDisabled(
+                        widget.notificationPreferences.warningSource)
+                    ? Switch(
+                        value:
+                            widget.notificationPreferences.notificationLevel !=
+                                NotificationLevel.disabled,
+                        onChanged: (value) {
+                          // if source is enabled, set notificationLevel to getUpToMinor
+                          // if source is disabled set notification level to disabled,
+                          // the slider will be hidden when notification
+                          // level is set to disabled
+                          if (value) {
+                            setState(() {
+                              widget.notificationPreferences.notificationLevel =
+                                  NotificationLevel.getUpToMinor;
+                            });
+                          } else {
+                            setState(() {
+                              widget.notificationPreferences.notificationLevel =
+                                  NotificationLevel.disabled;
+                            });
+                          }
+                          saveSettings();
+                        },
+                      )
+                    : SizedBox(),
+              ],
+            ),
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                    getDescriptionForEventSetting(
+                        widget.notificationPreferences.warningSource),
+                    style: Theme.of(context).textTheme.bodyMedium),
+                // hide the slider when source is disabled
+                SourceCanBeDisabled(
+                            widget.notificationPreferences.warningSource) &&
+                        widget.notificationPreferences.notificationLevel ==
+                            NotificationLevel.disabled
+                    ? Container(
+                        padding: EdgeInsets.only(top: 5),
+                        child: Text(
+                            "Source disabled - you won't get a notification"), //@todo translate
+                      )
+                    : Column(
                         children: [
-                          Text("Extrem"), // @todo translation
-                          Text("Schwer"),
-                          Text("Mittel"),
-                          Text("Gering"),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              Icon(
+                                Icons.notifications_active,
+                                color: Colors.red,
+                              ),
+                              Flexible(
+                                child: Slider(
+                                  label: getLabelForWarningSeverity(
+                                      getValueForWarningLevel(widget
+                                          .notificationPreferences
+                                          .notificationLevel)),
+                                  divisions: 3,
+                                  min: 0,
+                                  max: 3,
+                                  value: getValueForWarningLevel(widget
+                                      .notificationPreferences
+                                      .notificationLevel),
+                                  onChanged: (value) {
+                                    setState(
+                                      () {
+                                        print(widget.notificationPreferences
+                                            .warningSource.name);
+                                        // update notification level with slider value
+                                        widget.notificationPreferences
+                                                .notificationLevel =
+                                            getNotificationPreferencesForValue(
+                                                value);
+                                      },
+                                    );
+                                  },
+                                  onChangeEnd: (value) {
+                                    // save settings, after change is complete
+                                    saveSettings();
+                                  },
+                                ),
+                              ),
+                              Icon(
+                                Icons.notifications,
+                                color: Colors.orangeAccent,
+                              ),
+                            ],
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 30, right: 30),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text("Extrem"), // @todo translation
+                                Text("Schwer"),
+                                Text("Mittel"),
+                                Text("Gering"),
+                              ],
+                            ),
+                          )
                         ],
                       ),
-                    )
-                  ],
-                ),
-        ],
-      ),
-    );
+              ],
+            ),
+          );
   }
 }
