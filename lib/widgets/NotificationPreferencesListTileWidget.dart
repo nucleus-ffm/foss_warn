1c53c2f016417db6c071c60cf312199863431b20
Nucleus <nucleus-ffm@posteo.de>
2023-12-27 17:55:34 +0100

    Change the label of the slider to obtain a descending order of the notification settings

--- lib/widgets/NotificationPreferencesListTileWidget.dart
+++ lib/widgets/NotificationPreferencesListTileWidget.dart
@@ -26,23 +26,23 @@ class _NotificationPreferencesListTileWidgetState
     switch (sliderValue.toInt()) {
       case 0:
         return AppLocalizations.of(context)!
-            .notification_settings_notify_by_minor;
+            .notification_settings_notify_by_extreme;
       case 1:
         return AppLocalizations.of(context)!
-            .notification_settings_notify_by_moderate;
+            .notification_settings_notify_by_severe;
       case 2:
         return AppLocalizations.of(context)!
-            .notification_settings_notify_by_severe;
+            .notification_settings_notify_by_moderate;
       case 3:
         return AppLocalizations.of(context)!
-            .notification_settings_notify_by_extreme;
+            .notification_settings_notify_by_minor;
       default:
         return "Error";
     }
   }
 
   /// return the description for the given source
-  String getDescriptionForEventSetting(WarningSource source) {
+  String _getDescriptionForEventSetting(WarningSource source) {
     switch (source) {
       case WarningSource.dwd:
         return AppLocalizations.of(context)!.source_dwd_description;
@@ -65,7 +65,7 @@ class _NotificationPreferencesListTileWidgetState
   }
 
   /// decide if a source can be disabled
-  bool isTogglableSource(WarningSource source) {
+  bool _isTogglableSource(WarningSource source) {
     switch (source) {
       case WarningSource.dwd:
         return true;
@@ -86,122 +86,120 @@ class _NotificationPreferencesListTileWidgetState
 
   @override
   Widget build(BuildContext context) {
-    return widget.notificationPreferences.warningSource ==
-                WarningSource.alertSwiss &&
-            !userPreferences.activateAlertSwiss
-        ? SizedBox() // hide alert swiss if not activated
-        : ListTile(
-            contentPadding: settingsTileListPadding,
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  widget.notificationPreferences.warningSource.name
-                      .toUpperCase(),
-                  style: Theme.of(context).textTheme.titleMedium,
-                ),
-                // show a switch when source can be disabled
-                isTogglableSource(widget.notificationPreferences.warningSource)
-                    ? Switch(
-                        value:
-                            widget.notificationPreferences.disabled,
-                        onChanged: (value) {
-                          // the slider will be hidden when source is disabled
-                          if (value) {
-                            setState(() {
-                              widget.notificationPreferences.disabled = false;
-                            });
-                          } else {
-                            setState(() {
-                              widget.notificationPreferences.disabled = true;
-                            });
-                          }
-                          saveSettings();
-                        },
-                      )
-                    : SizedBox(),
-              ],
+    if (widget.notificationPreferences.warningSource ==
+            WarningSource.alertSwiss &&
+        !userPreferences.activateAlertSwiss) {
+      return SizedBox();
+    } else {
+      return ListTile(
+        contentPadding: settingsTileListPadding,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              widget.notificationPreferences.warningSource.name.toUpperCase(),
+              style: Theme.of(context).textTheme.titleMedium,
             ),
-            subtitle: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                    getDescriptionForEventSetting(
-                        widget.notificationPreferences.warningSource),
-                    style: Theme.of(context).textTheme.bodyMedium),
-                // hide the slider when source is disabled
-                isTogglableSource(widget.notificationPreferences.warningSource) &&
-                        widget.notificationPreferences.disabled
-                    ? Container(
-                        padding: EdgeInsets.only(top: 5),
-                        child: Text(
-                            "Source disabled - you won't get a notification"), //@todo translate
-                      )
-                    : Column(
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            mainAxisSize: MainAxisSize.max,
-                            children: [
-                              Icon(
-                                Icons.notifications_active,
-                                color: Colors.red,
-                              ),
-                              Flexible(
-                                child: Slider(
-                                  label: getLabelForWarningSeverity(
-                                      getIndexFromSeverity(widget
-                                          .notificationPreferences
-                                          .notificationLevel)),
-                                  divisions: 3,
-                                  min: 0,
-                                  max: 3,
-                                  value: getIndexFromSeverity(widget
-                                      .notificationPreferences
-                                      .notificationLevel),
-                                  onChanged: (value) {
-                                    setState(
-                                      () {
-                                        final notificationLevel = Severity.values[value.toInt()];
-
-                                        print(widget.notificationPreferences
-                                            .warningSource.name + ":" + notificationLevel.toString());
-
-                                        // update notification level with slider value
-                                        widget.notificationPreferences
-                                                .notificationLevel =
-                                            notificationLevel;
-                                      },
-                                    );
-                                  },
-                                  onChangeEnd: (value) {
-                                    // save settings, after change is complete
-                                    saveSettings();
-                                  },
-                                ),
-                              ),
-                              Icon(
-                                Icons.notifications,
-                                color: Colors.orangeAccent,
-                              ),
-                            ],
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(left: 30, right: 30),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text("Gering"), // @todo translation
-                                Text("Mittel"),
-                                Text("extreme"),
-                                Text("severe")
-                              ],
-                            ),
-                          )
-                        ],
+            // show a switch when source can be disabled
+            if (_isTogglableSource(
+                widget.notificationPreferences.warningSource))
+              Switch(
+                value: !widget.notificationPreferences.disabled,
+                onChanged: (value) {
+                  // the slider will be hidden when source is disabled
+                  setState(() {
+                    widget.notificationPreferences.disabled = !value;
+                  });
+                  saveSettings();
+                },
+              )
+            else
+              SizedBox(),
+          ],
+        ),
+        subtitle: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+                _getDescriptionForEventSetting(
+                    widget.notificationPreferences.warningSource),
+                style: Theme.of(context).textTheme.bodyMedium),
+            // hide the slider when source is disabled
+            if (_isTogglableSource(
+                    widget.notificationPreferences.warningSource) &&
+                widget.notificationPreferences.disabled)
+              Container(
+                padding: EdgeInsets.only(top: 5),
+                child: Text(
+                    "Source disabled - you won't get a notification"), //@todo translate
+              )
+            else
+              Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Icon(
+                        Icons.notifications_active,
+                        color: Colors.red,
                       ),
-              ],
-            ),
-          );
+                      Flexible(
+                        child: Slider(
+                          label: getLabelForWarningSeverity(
+                              Severity.getIndexFromSeverity(widget
+                                  .notificationPreferences.notificationLevel)),
+                          divisions: 3,
+                          min: 0,
+                          max: 3,
+                          value: Severity.getIndexFromSeverity(
+                              widget.notificationPreferences.notificationLevel),
+                          onChanged: (value) {
+                            setState(
+                              () {
+                                final notificationLevel =
+                                    Severity.values[value.toInt()];
+
+                                print(widget.notificationPreferences
+                                        .warningSource.name +
+                                    ":" +
+                                    notificationLevel.toString());
+
+                                // update notification level with slider value
+                                widget.notificationPreferences
+                                    .notificationLevel = notificationLevel;
+                              },
+                            );
+                          },
+                          onChangeEnd: (value) {
+                            // save settings, after change is complete
+                            saveSettings();
+                          },
+                        ),
+                      ),
+                      Icon(
+                        Icons.notifications,
+                        color: Colors.orangeAccent,
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 30, right: 30),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("extreme"), // @todo translation
+                        Text("severe"),
+                        Text("moderate"),
+                        Text("minor"),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+          ],
+        ),
+      );
+    }
   }
 }
