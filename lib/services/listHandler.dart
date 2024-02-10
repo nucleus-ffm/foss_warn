f7fcbf4591afc1b45fca38a9605147ba31063416
Nucleus <nucleus-ffm@posteo.de>
2024-02-10 22:30:17 +0100

    delete unused list and clarify name of other list

--- lib/services/listHandler.dart
+++ lib/services/listHandler.dart
@@ -3,9 +3,7 @@ import '../class/abstract_Place.dart';
 
 List<Place> myPlaceList = [];
 List<String> notificationSettingsImportance = [];
-// where the warnings for my places are stored
-List<WarnMessage> warnMessageList = [];
 // used if showAllWarnings is enabled to store all warnings
-List<WarnMessage> allWarnMessageList = [];
+List<WarnMessage> mapWarningsList = [];
 Map<String, String> geocodeMap = new Map();
 List<Place> allAvailablePlacesNames = [];
