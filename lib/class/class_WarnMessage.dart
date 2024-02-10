0dcef377354a55e89c99ab32c825828eb1bf7bda
Nucleus <nucleus-ffm@posteo.de>
2024-02-10 22:22:37 +0100

    implement CAP structure

--- lib/class/class_WarnMessage.dart
+++ lib/class/class_WarnMessage.dart
@@ -1,37 +1,46 @@
+import 'package:foss_warn/class/class_References.dart';
+import 'package:foss_warn/enums/Certainty.dart';
+import 'package:foss_warn/enums/Urgency.dart';
 import 'package:foss_warn/enums/WarningSource.dart';
-
-import '../enums/Certainty.dart';
+import '../enums/MessageType.dart';
+import '../enums/Scope.dart';
 import '../enums/Severity.dart';
+import '../enums/Status.dart';
 import 'class_Area.dart';
-import '../services/createAreaListFromJson.dart';
+import 'class_Info.dart';
 
 class WarnMessage {
   final String identifier;
   final String publisher;
-  final WarningSource source;
-  final String sender;
-  final String sent;
-  final String status;
-  final String messageType;
-  final String scope;
-  final String category;
-  final String event;
-  final String urgency;
-  final Severity severity;
-  final Certainty certainty;
-  final String headline;
-  final String description;
-  final String instruction;
-  final List<Area> areaList;
-  final String contact;
-  final String web;
-  // specific for warnings from DWD
-  final String effective;
-  final String onset;
-  final String expires;
+  final String
+      sender; // (REQUIRED) The identifier  of the sender of the alertmessage
+  final String
+      sent; // (REQUIRED) The time and date of the origination of the alert   message
+  final Status
+      status; // (REQUIRED) The code denoting the appropriate handling of the alert message
+  final MessageType
+      messageType; // (REQUIRED) The code denoting the nature of the alert message
+  final WarningSource
+      source; // (OPTIONAL) The text identifying the source of the alert message
+  final Scope
+      scope; // (REQUIRED) The code denoting the intended distribution ofthe alert message
+  String? restriction; // (CONDITIONAL) Used when <scope> value is "Restricted".
+  String?
+      addresses; // (CONDITIONAL) Required when <scope> is “Private”, optional when <scope> isPublic” or “Restricted”.
+  String?
+      code; // (OPTIONAL) Any user-defined flag or special code used to flag the alert message for special handling.
+  String?
+      note; // (OPTIONAL) The message note is primarily intended for  use with <status> “Exercise” and<msgType> “Error”.
+  References?
+      references; // (OPTIONAL)  The message note is primarily intended for use with <status> “Exercise” and <msgType> “Error”.
+  String?
+      incidents; // (OPTIONAL) Used to collate multiple messages referring to different aspects of the same incident.
+  final List<Info>
+      info; //  (OPTIONAL) The containerfor all component parts of the info sub-element of the alertmessage
 
   bool notified = false;
   bool read = false;
+  bool isUpdateOfAlreadyNotifiedWarning = false;
 
   WarnMessage({
     required this.identifier,
@@ -42,50 +51,36 @@ class WarnMessage {
     required this.status,
     required this.messageType,
     required this.scope,
-    required this.category,
-    required this.event,
-    required this.urgency,
-    required this.severity,
-    required this.certainty,
-    this.effective = "",
-    this.onset = "",
-    this.expires = "",
-    required this.headline,
-    required this.description,
-    required this.instruction,
-    required this.areaList,
-    required this.contact,
-    this.web = "",
+    restriction,
+    addresses,
+    code,
+    note,
+    references,
+    incidents,
+    required this.info,
     required this.notified,
     required this.read,
+    isUpdateOfAlreadyNotifiedWarning,
   });
 
   factory WarnMessage.fromJson(Map<String, dynamic> json) {
     return WarnMessage(
-        identifier: json['identifier'],
-        publisher: json['publisher'],
-        source: WarningSource.fromString(json['source'].toString()),
-        sender: json['sender'],
-        sent: json['sent'],
-        status: json['status'],
-        messageType: json['messageType'],
-        scope: json['scope'],
-        category: json['category'],
-        event: json['event'],
-        urgency: json['urgency'],
-        severity: Severity.fromJson(json['severity']),
-        certainty: Certainty.fromJson(json['certainty']),
-        effective: json['effective'],
-        onset: json['onset'],
-        expires: json['expires'],
-        headline: json['headline'],
-        description: json['description'],
-        instruction: json['instruction'],
-        areaList: areaListFromJson(json['areaList']),
-        contact: json['contact'],
-        web: json['web'] ?? "",
-        notified: json['notified'] as bool,
-        read: json['read'] as bool
+      identifier: json['identifier'] ?? '?',
+      publisher: json['publisher'] ?? "?",
+      source: WarningSource.fromString(json['source'].toString()),
+      sender: json['sender'] ?? "?",
+      sent: json['sent'] ?? "?",
+      status: Status.fromJson(json['status']),
+      messageType: MessageType.fromJson(json['messageType']),
+      scope: Scope.fromJson(json['scope']),
+      info: Info.infoListFromJson(json['info']),
+      notified: json['notified'] ?? false, //@todo check
+      read: json['read'] ?? false,
+      references: json['references'] != null
+          ? References.fromString(json['references'])
+          : null,
+      isUpdateOfAlreadyNotifiedWarning:
+          json['isUpdateOfAlreadyNotifiedWarning'] ?? false,
     );
   }
 
@@ -93,31 +88,21 @@ class WarnMessage {
   /// Note that the json structure is different from the structure we use to
   /// cache the warnings.
   factory WarnMessage.fromJsonTemp(Map<String, dynamic> json, String provider,
-      String publisher, List<Area> areaList) {
-    print("Neue WarnMessage wird angelegt...");
+      String publisher, List<dynamic> coordinates) {
+    // print("Neue WarnMessage wird angelegt...");
     return WarnMessage(
         source: WarningSource.fromString(provider),
         identifier: json["identifier"] ?? "?",
         sender: json["sender"] ?? "?",
         sent: json["sent"] ?? "?",
-        status: json["status"] ?? "?",
-        messageType: json["msgType"] ?? "?",
-        scope: json["scope"] ?? "?",
-        category: json["info"][0]["category"][0] ?? "?",
-        event: json["info"][0]["event"] ?? "?",
-        urgency: json["info"][0]["urgency"] ?? "?",
-        severity: Severity.fromString(json["info"][0]["severity"].toString().toLowerCase()),
-        certainty: getCertainty(json["info"][0]["certainty"].toString().toLowerCase()),
-        effective: json["info"][0]["effective"] ?? "",
-        onset: json["info"][0]["onset"] ?? "",
-        expires: json["info"][0]["expires"] ?? "",
-        headline: json["info"][0]["headline"] ?? "?",
-        description: json["info"][0]["description"] ?? "",
-        instruction: json["info"][0]["instruction"] ?? "",
+        status: Status.fromJson(json["status"]),
+        messageType: MessageType.fromJson(json["msgType"]),
+        scope: Scope.fromJson(json["scope"]),
         publisher: publisher,
-        contact: json["info"][0]["contact"] ?? "",
-        web: json["info"][0]["web"] ?? "",
-        areaList: areaList,
+        info: Info.infoListFromJsonTemp(json['info'], coordinates),
+        references: json['references'] != null
+            ? References.fromString(json['references'])
+            : null,
         notified: false,
         read: false);
   }
@@ -132,24 +117,22 @@ class WarnMessage {
         identifier: json["identifier"] ?? "?",
         sender: json["sender"] ?? "?",
         sent: json["sent"] ?? "?",
-        status: "?", // missing for alert swiss
-        messageType: "Alert", // missing
-        scope: "?", // missing
-        category: json["event"] ?? "?", // missing
-        event: json["event"] ?? "?",
-        urgency: "?",
-        severity: Severity.fromString(json["severity"]),
-        certainty: getCertainty(""), // missing
-        effective: "", // missing
-        onset: json["onset"] ?? "", // m
-        expires: json["expires"] ?? "", // m
-        headline: json["title"]["title"] ?? "?",
-        description: json["description"]["description"] ?? "",
-        instruction: instructions,
+        status: Status.Actual, // missing for alert swiss
+        messageType: MessageType.Alert, // missing
+        scope: Scope.Public, // missing
         publisher: license,
-        contact: json["contact"]["contact"] ?? "",
-        web: json["link"] ?? "",
-        areaList: areaList,
+        info: [
+          Info(
+              category: [],
+              event: json["event"] ?? "",
+              urgency: Urgency.Unknown,
+              severity: Severity.fromString(json["severity"]),
+              certainty: Certainty.Unknown,
+              headline: json["title"]["title"] ?? "?",
+              description: json["description"]["description"] ?? "",
+              instruction: instructions,
+              area: areaList)
+        ],
         notified: false,
         read: false);
   }
@@ -163,20 +146,10 @@ class WarnMessage {
         'status': status,
         'messageType': messageType,
         'scope': scope,
-        'category': category,
-        'event': category,
-        'urgency': urgency,
-        'severity': severity,
-        'certainty': certainty,
-        'effective': effective,
-        'onset': onset,
-        'expires': expires,
-        'headline': headline,
-        'description': description,
-        'instruction': instruction,
-        'areaList': areaList,
-        'contact': contact,
         'notified': notified,
-        'read': read
+        'read': read,
+        'info': info,
+        'references': references,
+        'isUpdateOfAlreadyNotifiedWarning': isUpdateOfAlreadyNotifiedWarning
       };
 }
