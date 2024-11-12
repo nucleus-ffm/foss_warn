950df2b2fd66c81cc1f60a079436744fc785e2b4
Nucleus <nucleus-ffm@posteo.de>
2024-11-12 13:43:44 +0100

    move createAllPolygons method to area class

--- lib/widgets/MapWidget.dart
+++ lib/widgets/MapWidget.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:flutter_map_geojson/flutter_map_geojson.dart';
-import 'package:foss_warn/class/class_ErrorLogger.dart';
 
 import '../class/abstract_Place.dart';
 import '../class/class_Area.dart';
@@ -23,28 +21,6 @@ class MapWidget extends StatefulWidget {
       required this.mapController,
       required this.initialCameraFit});
 
-  /// create a list of polygons from a list of areas
-  //  default color: 0xFFB01917
-  //  default borderColor: 0xFFFB8C00
-  static List<Polygon> createAllPolygons(List<Area> areas) {
-    List<Polygon> result = [];
-    try {
-      GeoJsonParser myGeoJson = GeoJsonParser(
-          defaultPolygonFillColor: Color(0xFFB01917).withOpacity(0.2),
-          defaultPolygonBorderColor: Color(0xFFFB8C00),
-          defaultPolylineStroke: 1);
-      for (Area area in areas) {
-        myGeoJson.parseGeoJsonAsString(area.geoJson);
-        result.addAll(myGeoJson.polygons);
-      }
-      return result;
-    } catch (e) {
-      ErrorLogger.writeErrorLog("MapWidget", "Error while parsing geoJson", e.toString());
-      appState.error = true;
-      return [];
-    }
-  }
-
   /// create polygon layer for my places alerts
   static List<PolygonLayer> createPolygonLayer() {
     List<PolygonLayer> result = [];
@@ -52,7 +28,7 @@ class MapWidget extends StatefulWidget {
       for (WarnMessage wm in p.warnings) {
         result.add(
           PolygonLayer(
-            polygons: createAllPolygons(wm.info.first.area),
+            polygons: Area.createListOfPolygonsForAreas(wm.info.first.area),
           ),
         );
       }
@@ -66,7 +42,7 @@ class MapWidget extends StatefulWidget {
       result.add(
         PolygonLayer(
           polygonCulling: true,
-          polygons: MapWidget.createAllPolygons(wm.info.first.area),
+          polygons: Area.createListOfPolygonsForAreas(wm.info.first.area),
         ),
       );
     }
@@ -100,8 +76,8 @@ class _MapWidgetState extends State<MapWidget> {
               flags: InteractiveFlag.all & ~InteractiveFlag.rotate)),
       children: [
         TileLayer(
-          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-          userAgentPackageName: 'de.nucleus.foss_warn',
+          urlTemplate: userPreferences.osmTileServerULR,
+          userAgentPackageName: userPreferences.httpUserAgent,
           tileBuilder:
               (BuildContext context, Widget tileWidget, TileImage tile) {
             // there is not build in dark mode with the tiles form osm.org therefore we
