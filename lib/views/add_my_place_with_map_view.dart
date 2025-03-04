c07b49fc9d4c832117df91f54841ad3339c92e3c
Nucleus <nucleus-ffm@posteo.de>
2025-03-04 23:16:55 +0100

    replace deprecated "withOpacity" call with "withValues"

--- lib/views/add_my_place_with_map_view.dart
+++ lib/views/add_my_place_with_map_view.dart
@@ -209,7 +209,7 @@ class _AddMyPlaceWithMapViewState extends ConsumerState<AddMyPlaceWithMapView> {
     );
     // create polygon around place
     selectedPlacePolygon = Polygon(
-      color: Theme.of(context).colorScheme.secondary.withOpacity(0.5),
+      color: Theme.of(context).colorScheme.secondary.withValues(alpha: .5),
       points: circlePolygonPoints,
     );
   }
@@ -224,7 +224,7 @@ class _AddMyPlaceWithMapViewState extends ConsumerState<AddMyPlaceWithMapView> {
     );
     // create polygon around place
     selectedPlacePolygon = Polygon(
-      color: Theme.of(context).colorScheme.secondary.withOpacity(0.5),
+      color: Theme.of(context).colorScheme.secondary.withValues(alpha: 0.5),
       points: points,
     );
   }
@@ -511,6 +511,7 @@ class _AddMyPlaceWithMapViewState extends ConsumerState<AddMyPlaceWithMapView> {
                               // setup unifiedPush
                               await UnifiedPushHandler.setupUnifiedPush(
                                 context,
+                                ref,
                               );
 
                               // subscribe for new area and create new place
