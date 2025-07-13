a3f6fedf66e733f9ad21d9bc06dd75d98258d252
Nucleus <nucleus-ffm@posteo.de>
2025-07-13 12:41:06 +0200

    fix background notifications with a simpler procedure that avoids a concurrent modification error during iteration.

--- lib/class/class_unified_push_handler.dart
+++ lib/class/class_unified_push_handler.dart
@@ -1,9 +1,12 @@
 import 'dart:convert';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:foss_warn/class/class_error_logger.dart';
 import 'package:foss_warn/class/class_user_preferences.dart';
+import 'package:foss_warn/class/class_warn_message.dart';
 import 'package:foss_warn/services/api_handler.dart';
 import 'package:foss_warn/services/list_handler.dart';
 import 'package:foss_warn/services/warnings.dart';
@@ -12,6 +15,7 @@ import 'package:unifiedpush/unifiedpush.dart';
 import '../services/alert_api/fpas.dart';
 import '../widgets/dialogs/no_up_distributor_found_dialog.dart';
 import '../widgets/dialogs/select_unified_push_distributor_dialog.dart';
+import 'class_notification_service.dart';
 
 final unifiedPushHandlerProvider = Provider(
   (ref) => UnifiedPushHandler(
@@ -56,9 +60,12 @@ class UnifiedPushHandler {
   }
 
   void onUnregistered(String instance) {
+    debugPrint("onUnregistered called");
     _preferencesService.setUnifiedpushEndpoint("");
     _preferencesService.setUnifiedPushRegistered(false);
-    _preferencesService.setWebPushVapidKey(null);
+    _preferencesService.setWebPushVapidKey("");
+    _preferencesService.setWebPushAuthKey("");
+    _preferencesService.setWebPushPublicKey("");
   }
 
   /// callback function to handle notification from unifiedPush
@@ -75,15 +82,56 @@ class UnifiedPushHandler {
 
     var payload = utf8.decode(message.content);
     debugPrint("Received a notification. Message: $payload");
-
-    var alerts = ref.refresh(processedAlertsProvider);
-    var alertsService = ref.read(processedAlertsProvider.notifier);
-    var places = ref.read(myPlacesProvider);
     var userPreferences = ref.read(userPreferencesProvider);
-    if (!context.mounted) {
-      return;
+
+    if (userPreferences.showDebugNotification) {
+      NotificationService.showNotification(
+        id: Random().nextInt(100),
+        title: "DEBUG Notification",
+        body:
+            "FOSSWarn has received a push notification with content: $payload",
+        payload: "",
+        channelId: "de.nucleus.foss_warn.notifications_other",
+        channelName: "Debug notifications",
+      );
     }
-    showNotification(alerts, places, userPreferences, context, alertsService);
+
+    String? addedAlertId;
+
+    try {
+      var payloadAsJson = jsonDecode(payload) as Map<String, dynamic>;
+      addedAlertId = payloadAsJson["alert_id"];
+      if (addedAlertId != null) {
+        WarnMessage alert = await ref.read(alertApiProvider).getAlertDetail(
+              alertId: addedAlertId,
+              placeSubscriptionId: "Not used",
+            );
+        if (checkIfEventShouldBeNotified(
+          alert.info[0].severity,
+          userPreferences,
+        )) {
+          NotificationService.showNotification(
+            id: alert.identifier.hashCode,
+            title: "New alert",
+            body: alert.info.first.headline,
+            payload: "",
+            channelId:
+                "de.nucleus.foss_warn.notifications_${alert.info[0].severity.name}",
+            channelName: "",
+          );
+        }
+      }
+    } on FormatException catch (e) {
+      debugPrint("Payload is not a json: $e");
+    } on AlertUnavailableError catch (e) {
+      debugPrint("Alert is not available anymore: $e");
+      ErrorLogger.writeErrorLog(
+        "class_unified_push_handler.dart",
+        "onMessage",
+        "Alert $addedAlertId is not available anymore",
+      );
+    }
+    // @TODO(Nucleus): This is not the perfect solution as we can not check for already read alerts or if the alert is just an update. It would be preferable if we could fetch all alerts like we did before. Currently, this results in a "concurrent modification during iteration" error if the app is launched by a notification.
   }
 
   /// register for push notifications and keep registration up to date
@@ -95,16 +143,22 @@ class UnifiedPushHandler {
     // fetch fresh server config to check which push services are supported
     AlertAPI alertAPI = ref.read(alertApiProvider);
     ServerSettings serverSettings = await alertAPI.fetchServerSettings();
-    // check if webpush / encrypted UP is supported
+    // check if WebPush / encrypted UP is supported
     bool isEncryptedUnifiedPushSupported =
         serverSettings.supportedPushServices["UNIFIED_PUSH_ENCRYPTED"] ?? false;
 
-    // if server supports webpush, try to fetch the vapid key
-    if (_userPreferences.webPushVapidKey == null &&
+    // Used to access the vapid key after a fresh fetch for some reason,
+    // _userPreferences.webPushVapidKey is still empty after we called
+    // setWebPushVapidKey. It takes some time until _userPreferences.webPushVapidKey
+    // has the correct value.
+    String? tempVapidKey;
+
+    // if server supports WebPush, try to fetch the vapid key
+    if (_userPreferences.webPushVapidKey == "" &&
         isEncryptedUnifiedPushSupported) {
       try {
-        _preferencesService
-            .setWebPushVapidKey(await alertAPI.fetchVapidKeyForWebPush());
+        tempVapidKey = await alertAPI.fetchVapidKeyForWebPush();
+        _preferencesService.setWebPushVapidKey(tempVapidKey);
       } on VapidKeyException {
         isEncryptedUnifiedPushSupported = false;
         ErrorLogger.writeErrorLog(
@@ -122,7 +176,7 @@ class UnifiedPushHandler {
       await UnifiedPush.register(
         instance: UserPreferences
             .unifiedPushInstance, // Optional String, to get multiple endpoints (one per instance)
-        vapid: _userPreferences.webPushVapidKey,
+        vapid: tempVapidKey ?? _userPreferences.webPushVapidKey,
       );
     } else {
       // Get a list of distributors that are available
@@ -149,11 +203,16 @@ class UnifiedPushHandler {
       // save the distributor
       await UnifiedPush.saveDistributor(picked ?? distributors.first);
       // register your app to the distributor
-      await UnifiedPush.register(
-        instance: UserPreferences
-            .unifiedPushInstance, // optional String, to get multiple endpoints (one per instance)
-        vapid: _userPreferences.webPushVapidKey,
-      );
+      try {
+        await UnifiedPush.register(
+          instance: UserPreferences.unifiedPushInstance,
+          // optional String, to get multiple endpoints (one per instance)
+          vapid: tempVapidKey ?? _userPreferences.webPushVapidKey,
+        );
+      } on MissingPluginException catch (e) {
+        debugPrint("error while registering UnifiedPush: $e");
+        return;
+      }
       return;
     }
   }
