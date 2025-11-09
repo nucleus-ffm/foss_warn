fe8fc41100b429569140e65dbc4b6a0e5fb59d66
Nucleus <nucleus-ffm@posteo.de>
2025-11-09 15:32:52 +0100

    move onTap callback to own method and display an information if there are not distributors found

--- lib/widgets/dialogs/change_unified_push_distributor_dialog.dart
+++ lib/widgets/dialogs/change_unified_push_distributor_dialog.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:foss_warn/class/class_user_preferences.dart';
 import 'package:foss_warn/extensions/context.dart';
-import 'package:unifiedpush/unifiedpush.dart';
 import '../../class/class_unified_push_handler.dart';
 import '../../services/subscription_handler.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+import 'loading_screen.dart';
 
 class ChangeUnifiedPushDistributorDialog extends ConsumerStatefulWidget {
   const ChangeUnifiedPushDistributorDialog({super.key});
@@ -18,20 +17,50 @@ class ChangeUnifiedPushDistributorDialog extends ConsumerStatefulWidget {
 
 class _ChangeUnifiedPushDistributorDialogState
     extends ConsumerState<ChangeUnifiedPushDistributorDialog> {
-  /// register with the selected distributor for push notifications
-  Future<void> registerDistributor(String picked, WidgetRef ref) async {
-    UserPreferences userPreferences = ref.read(userPreferencesProvider);
-    await UnifiedPush.saveDistributor(picked);
-    // register your app to the distributor
-    try {
-      await UnifiedPush.register(
-        instance: UserPreferences.unifiedPushInstance,
-        vapid: userPreferences.webPushVapidKey,
-      );
-    } on MissingPluginException catch (e) {
-      debugPrint("error while registering UnifiedPush: $e");
+  /// handle the onTap call for every distributorListTile
+  /// changes the distributor
+  Future<void> onDistributorTapped(Map<String, String> d) async {
+    var localizations = context.localizations;
+    LoadingScreen.instance().show(
+      context: context,
+      text: localizations
+          .change_unified_push_distributor_dialog_loading_screen_change_distributor,
+    );
+    // register distributor if one is selected, otherwise do nothing
+    if (d["distributor"] != null) {
+      UnifiedPushHandler unifiedPushHandler =
+          ref.watch(unifiedPushHandlerProvider);
+
+      try {
+        await unifiedPushHandler.changeDistributor(d["distributor"]!, ref);
+      } on UnifiedPushRegistrationTimeoutError {
+        if (!mounted) {
+          return;
+        }
+        LoadingScreen.instance().showResult(
+          text: localizations
+              .change_unified_push_distributor_dialog_loading_screen_timeout_error,
+        );
+        debugPrint(
+          "[changeUnifiedPushDistributor] UnifiedPush registration failed",
+        );
+
+        Navigator.pop(context);
+        return;
+      }
+    }
+
+    if (!mounted) {
       return;
     }
+
+    // resubscribe for all subscription
+    await resubscribeForAllArea(context, ref);
+
+    if (!mounted) {
+      return;
+    }
+    Navigator.pop(context, d["distributor"]);
   }
 
   /// build the selection ListTiles
@@ -50,56 +79,23 @@ class _ChangeUnifiedPushDistributorDialogState
                 localizations.change_unified_push_distributor_dialog_body,
               ),
             ),
-            ...distributors.map<Widget>(
-              (d) => Padding(
-                padding: const EdgeInsets.all(3.0),
-                child: ListTile(
-                  title: Text("${d["name"]} (${d["distributor"]})"),
-                  leading: const Icon(Icons.public),
-                  onTap: () async {
-                    // register distributor is one is selected, otherwise do nothing
-                    if (d["distributor"] != null) {
-                      UserPreferencesService userPreferencesService =
-                          ref.read(userPreferencesProvider.notifier);
-                      userPreferencesService.setUnifiedPushRegistered(false);
-                      UserPreferences userPreferences =
-                          ref.watch(userPreferencesProvider);
-
-                      await registerDistributor(d["distributor"]!, ref);
-
-                      // wait until the registration is finished we have a new endpoint
-                      await Future.doWhile(() async {
-                        await Future.delayed(const Duration(microseconds: 1));
-                        userPreferences = ref.read(userPreferencesProvider);
-                        return !userPreferences.unifiedPushRegistered;
-                      }).timeout(
-                        const Duration(seconds: 20),
-                        onTimeout: () {
-                          debugPrint(
-                            "Timeout waiting for unifiedPushRegistered to be set to true.",
-                          );
-                          return;
-                        },
-                      );
-                    }
-
-                    if (!mounted) {
-                      return;
-                    }
-
-                    //@TODO we have to wait until the push notification configuration is finished
-
-                    // resubscribe for all subscription
-                    await resubscribeForAllArea(context, ref);
-
-                    if (!mounted) {
-                      return;
-                    }
-                    Navigator.pop(context, d["distributor"]);
-                  },
-                ),
-              ),
-            ),
+            ...distributors.isNotEmpty
+                ? distributors.map<Widget>(
+                    (d) => Padding(
+                      padding: const EdgeInsets.all(3.0),
+                      child: ListTile(
+                        title: Text("${d["name"]} (${d["distributor"]})"),
+                        leading: const Icon(Icons.public),
+                        onTap: () => onDistributorTapped(d),
+                      ),
+                    ),
+                  )
+                : [
+                    Text(
+                      localizations
+                          .change_unified_push_distributor_dialog_body_no_distributor_found,
+                    ),
+                  ],
           ],
         ),
       ],
