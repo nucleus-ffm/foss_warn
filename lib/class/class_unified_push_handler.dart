d7dcf75a008cd336c0eba93d6a4e060b5f105aec
Nucleus <nucleus-ffm@posteo.de>
2025-11-09 15:47:59 +0100

    implement method to change the distributor

--- lib/class/class_unified_push_handler.dart
+++ lib/class/class_unified_push_handler.dart
@@ -10,10 +10,14 @@ import 'package:foss_warn/services/list_handler.dart';
 import 'package:foss_warn/services/warnings.dart';
 
 import 'package:unifiedpush/unifiedpush.dart';
+import '../main.dart';
 import '../services/alert_api/fpas.dart';
 import '../services/notification_handler.dart';
 import '../widgets/dialogs/no_up_distributor_found_dialog.dart';
 import '../widgets/dialogs/select_unified_push_distributor_dialog.dart';
+
+/// Thrown when a UnifiedPush registration failed with a timeout
+class UnifiedPushRegistrationTimeoutError implements Exception {}
 
 final unifiedPushHandlerProvider = Provider(
   (ref) => UnifiedPushHandler(
@@ -32,12 +36,13 @@ class UnifiedPushHandler {
   final UserPreferencesService _preferencesService;
   final UserPreferences _userPreferences;
 
+  /// Callback function for the UnifiedPush  plugin
+  /// this method gets called when a new endpoint is selected
   void onNewEndpoint(PushEndpoint endpoint, String instance) {
     debugPrint("new Endpoint:${endpoint.url}");
-    if (instance != UserPreferences.unifiedPushInstance) {
-      return;
-    }
+    if (instance != UserPreferences.unifiedPushInstance) return;
 
+    // update preferences with the new URL and Keys
     _preferencesService.setUnifiedpushEndpoint(endpoint.url);
     if (endpoint.pubKeySet != null) {
       _preferencesService.setWebPushPublicKey(endpoint.pubKeySet!.pubKey);
@@ -47,7 +52,11 @@ class UnifiedPushHandler {
     //@TODO(Nucleus): we need to update the subscriptions and send the new endpoint to the server
   }
 
+  /// Callback for the UnfiedPush plugin
+  /// This method gets called with the registration failed
+  /// For now we are just logging that error
   void onRegistrationFailed(FailedReason failedReason, String instance) {
+    if (instance != UserPreferences.unifiedPushInstance) return;
     // @todo error handling
     ErrorLogger.writeErrorLog(
       "class_unifiedPushHandler",
@@ -57,7 +66,13 @@ class UnifiedPushHandler {
     debugPrint("Registration failed: ${failedReason.name}");
   }
 
+  /// Callback for the UnifiedPush plugin
+  /// This method gets called when the client unregisters from the distributor
+  /// This updates the state in the preferences
   void onUnregistered(String instance) {
+    debugPrint("onUnregistered called");
+    if (instance != UserPreferences.unifiedPushInstance) return;
+
     debugPrint("onUnregistered called");
     _preferencesService.setUnifiedpushEndpoint("");
     _preferencesService.setUnifiedPushRegistered(false);
@@ -66,7 +81,8 @@ class UnifiedPushHandler {
     _preferencesService.setWebPushPublicKey("");
   }
 
-  /// callback function to handle notification from unifiedPush
+  /// callback for the UnifiedPush plugin
+  /// This method handles incoming notification from unifiedPush
   Future<void> onMessage({
     required AlertAPI alertApi,
     required MyPlacesService myPlacesService,
@@ -86,6 +102,7 @@ class UnifiedPushHandler {
 
   /// register for push notifications and keep registration up to date
   /// This method needs to called at every app startup
+  /// @TODO Nucleus: refactor this and use more smaller methods
   Future<void> setupUnifiedPush(
     BuildContext context,
     WidgetRef ref,
@@ -163,6 +180,83 @@ class UnifiedPushHandler {
         debugPrint("error while registering UnifiedPush: $e");
         return;
       }
+      return;
+    }
+  }
+
+  /// Change the current distributor to the new selected distributor
+  /// This method first unregisters the old distributor and then registers
+  /// again with the new selected Distributor
+  /// The selected distributor has to be the [String] id of the distributor
+  ///
+  /// Throws an [UnifiedPushRegistrationTimeoutError] when the registration failed
+  /// with en timeout error
+  Future<void> changeDistributor(
+    String selectedDistributor,
+    WidgetRef ref,
+  ) async {
+    appState.reSubscriptionInProgress = true;
+    await unregisterDistributor(ref);
+    await registerDistributor(selectedDistributor, ref);
+    // wait until the registration is finished we have a new endpoint
+    await Future.doWhile(() async {
+      await Future.delayed(const Duration(microseconds: 1));
+      UserPreferences userPreferences = ref.read(userPreferencesProvider);
+      return !userPreferences.unifiedPushRegistered;
+    }).timeout(
+      const Duration(seconds: 20),
+      onTimeout: () {
+        debugPrint(
+          "Timeout waiting for unifiedPushRegistered to be set to true.",
+        );
+        throw UnifiedPushRegistrationTimeoutError();
+      },
+    );
+  }
+
+  /// unregisters the current distributor and sets the unifiedPush
+  /// registered flag in the setting to false
+  Future<void> unregisterDistributor(WidgetRef ref) async {
+    UnifiedPush.unregister(UserPreferences.unifiedPushInstance);
+    await _preferencesService.setUnifiedPushRegistered(false);
+  }
+
+  /// Register for notifications with the given distributor
+  Future<void> registerDistributor(
+    String selectedDistributor,
+    WidgetRef ref,
+  ) async {
+    debugPrint(
+      "[unifiedPushHandler] register new distributor $selectedDistributor",
+    );
+    await UnifiedPush.saveDistributor(selectedDistributor);
+    String? tempVapidKey;
+    if (_userPreferences.webPushVapidKey == "") {
+      // if server supports WebPush, try to fetch the vapid key
+      if (_userPreferences.webPushVapidKey == "") {
+        try {
+          AlertAPI alertAPI = ref.read(alertApiProvider);
+          tempVapidKey = await alertAPI.fetchVapidKeyForWebPush();
+          _preferencesService.setWebPushVapidKey(tempVapidKey);
+        } on VapidKeyException {
+          ErrorLogger.writeErrorLog(
+            "class_unified_push_handler.dart",
+            "setup unifiedPush",
+            "Failed to fetch VAPID key for webpush",
+          );
+        }
+      }
+    }
+
+    // register your app to the distributor
+    try {
+      await UnifiedPush.register(
+        instance: UserPreferences.unifiedPushInstance,
+        vapid: tempVapidKey ?? _userPreferences.webPushVapidKey,
+      );
+    } on MissingPluginException catch (e) {
+      //@TODO (Nucleus) do not catch this exception here
+      debugPrint("error while registering UnifiedPush: $e");
       return;
     }
   }
