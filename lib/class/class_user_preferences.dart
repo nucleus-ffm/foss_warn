8682e501c06ad2da370ffcb8e5b3a4bf23fd8ac9
Nucleus <nucleus-ffm@posteo.de>
2026-05-08 09:03:20 +0200

    increase version code and number to 1.1.0-alpha1+43

--- lib/class/class_user_preferences.dart
+++ lib/class/class_user_preferences.dart
@@ -445,9 +445,9 @@ class UserPreferences {
   // Version of the application, shown in the about view
   // TODO(PureTryOut): get this from package_info_plus instead
   // That way we only need to keep track of one number.
-  static const String versionNumber = "1.1.0-alpha0";
+  static const String versionNumber = "1.1.0-alpha1";
 
-  static const int currentVersionCode = 42;
+  static const int currentVersionCode = 43;
   final int previousInstalledVersionCode;
 
   static const String unifiedPushInstance = "FOSSWarn";
