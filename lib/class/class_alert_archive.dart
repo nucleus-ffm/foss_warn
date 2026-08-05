ae91ffbfdc6001074a87973e5f2160b9990d5139
Nucleus <nucleus-ffm@posteo.de>
2026-08-05 19:05:13 +0200

    Migrate from bare string reading and writing to handling Json objects to prevent breaking the archive with broken Json

--- lib/class/class_alert_archive.dart
+++ lib/class/class_alert_archive.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:foss_warn/class/class_error_logger.dart';
 import 'package:foss_warn/class/class_user_preferences.dart';
 import 'package:foss_warn/class/class_warn_message.dart';
 import 'package:foss_warn/services/warnings.dart';
@@ -37,7 +38,7 @@ class AlertArchive {
   AlertArchive({required this.alertArchive});
 
   /// create a new alertArchive object
-  static Future<AlertArchive> create(WidgetRef ref) async {
+  static Future<AlertArchive> create(Ref ref) async {
     return AlertArchive(alertArchive: await readArchive(ref));
   }
 
@@ -70,29 +71,24 @@ class AlertArchive {
   }
 
   /// reads the archive and return the parse alerts
-  static Future<List<WarnMessage>> readArchive(WidgetRef ref) async {
+  static Future<List<WarnMessage>> readArchive(Ref ref) async {
     try {
       final file = await _localFile;
-      String archive = await file.readAsString();
+      String archive = await file.readAsString().catchError((_) => '[]');
+      final List<dynamic> jsonList = jsonDecode(archive) as List<dynamic>;
 
-      // there are not stored alerts
-      if (archive.isEmpty) return [];
-
-      // read the archive
-      List<String> alerts = archive.split(";");
       List<WarnMessage> result = [];
-      for (String alertString in alerts) {
-        if (alertString == "") continue;
-        Map<String, dynamic> alertJson = jsonDecode(alertString);
-        WarnMessage alert = WarnMessage.fromJson(
-          alertJson,
-          fpasId: alertJson['fpasId'],
+      for (final item in jsonList) {
+        if (item is! Map<String, dynamic>) continue;
+
+        final alert = WarnMessage.fromJson(
+          item,
+          fpasId: item['fpasId'],
           placeId: constants.noPlaceId,
         );
         ref.read(processedAlertsProvider.notifier).updateAlert(alert);
         result.add(alert);
       }
-
       return result;
     } catch (e) {
       // If encountering an error, return 0
@@ -107,24 +103,50 @@ class AlertArchive {
   }
 
   /// write the given alert to the archive
+  ///
+  /// We need some kind of mutex to prevent broken json files
   static Future<void> writeAlertToArchive(
     WarnMessage alert,
   ) async {
     // we might want to check if the new alert is not already stored
+    final file = await _localFile;
+    final raf =
+        await file.open(mode: FileMode.append); // do not truncate on open
+    await raf.lock(FileLock.blockingExclusive);
     try {
-      final file = await _localFile;
-      file.writeAsString(
-        // store the alert separated with a ;
-        "${jsonEncode(alert)};",
-        mode: FileMode.append,
-      );
+      // start reading from the beginning of the file
+      await raf.setPosition(0);
+      final length = await raf.length();
+      final bytes = length > 0 ? await raf.read(length) : <int>[];
+      final content = bytes.isNotEmpty ? utf8.decode(bytes) : '[]';
+
+      List<dynamic> alerts;
+      try {
+        alerts = jsonDecode(content) as List<dynamic>;
+      } catch (e) {
+        await ErrorLogger.writeLog(
+          "class_alert_archive.dart",
+          "writeAlertToArchive",
+          "Error while adding alert ${e.toString()}",
+        );
+        alerts = [];
+      }
+      alerts.add(alert.toJson());
+      final encoded = utf8.encode(jsonEncode(alerts));
+      // ensure that we start writing at the start of the file
+      await raf.setPosition(0);
+      await raf.truncate(0);
+      await raf.writeFrom(encoded);
     } catch (e) {
       debugPrint("Error while writing archive ${e.toString()}");
-      ErrorLogger.writeLog(
+      await ErrorLogger.writeLog(
         "class_alert_archive.dart",
         "writeAlertToArchive",
         "Error while writing archive ${e.toString()}",
       );
+    } finally {
+      await raf.unlock();
+      await raf.close();
     }
   }
 }
