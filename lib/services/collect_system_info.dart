a5c22ff302fd98db5a7498653a064a80b9214e1b
Nucleus <nucleus-ffm@posteo.de>
2026-08-19 22:04:46 +0200

    Add missing translation for the collect system information dialog

--- lib/services/collect_system_info.dart
+++ lib/services/collect_system_info.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:foss_warn/extensions/context.dart';
 
 /// collect all available infos to help finding Bugs and wrong settings
-Future<String> collectSystemInfo() async {
-  String result = "Systeminformationen \n\n";
-
-  result += "Akkuoptimierung: ${await _isBatteryOptimizationEnabled()} \n";
-
+Future<String> collectSystemInfo(BuildContext context) async {
+  var localizations = context.localizations;
+  String result = localizations.collect_system_information_system_information;
+  result += "\n\n";
+  result +=
+      "${localizations.collect_system_information_battery_optimization}: ${await _isBatteryOptimizationEnabled()} \n";
   return result;
 }
 
